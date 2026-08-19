e251dc473d97c749f955090b3759cff7e7bd74ee
emilymontec <emilymonterrosa.c@gmail.com>
2026-08-19 11:33:31 -0500

    Preparacion deploy

--- src/backend/shared/audit-log.ts
+++ src/backend/shared/audit-log.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client";
+import { Prisma } from "@/generated/prisma/client";
 import { prisma } from "@/src/backend/shared/prisma";
 import { logger } from "@/src/shared/lib/logger";
 
